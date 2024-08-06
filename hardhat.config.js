667779476f9a8e73244c13ee9897cce111d89d33
Your Name <youremail@gmail.com>
2024-08-06 03:48:32 +0000

    Initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/",
-      accounts: ["0x"], //Your private key starting with "0x"
+      accounts: [""],
     },
   },
 };
